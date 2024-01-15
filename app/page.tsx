dd416422ede2177330c2efbbbf5cf51f3a8ef52e
Benedikt Keller <benedikt.keller21@gmail.com>
2024-01-15 23:30:07 +0100

    added landing page beta

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,33 @@
 import Image from 'next/image'
 import Link from 'next/link'
+import './landing.css'
 
 export default function Home() {
   return (
-    <div style={{padding: '2rem'}}>
-      <Link href="/blog" style={{textDecoration: 'none'}}>
-        <h1>Blog</h1>
-      </Link>
+    <div style={{backgroundColor: 'white', overflow: 'hidden'}}>
+        <h1 style={{zIndex: '200', position: 'fixed', color: 'black', display: 'flex', width: '100%', justifyContent: 'center', paddingTop: '0.5rem'}}>
+          enter
+        </h1>
+        <div style={{zIndex: '2000', display: 'flex', justifyContent: 'center', alignItems: 'center',flexDirection: 'column', height: '100vh', overflow: 'hidden', padding: '1rem'}}>
+          <div className='panel-container' style={{overflow:'hidden'}}>
+          <Link href="/blog" style={{textDecoration: 'none'}} className='panel'>
+              <h5 className='panel-title'>blog</h5>
+          </Link>
+
+          <Link href="/ffield" style={{textDecoration: 'none'}} className='panel'>        
+              <h5 className='panel-title'>music</h5>
+          </Link>
+          </div>
+        
+        <h1 style={{zIndex: '200', position: 'fixed', color: 'black', display: 'flex', width: '100%', justifyContent: 'center', bottom: '0', paddingBottom: '0.5rem'}}>
+          the field
+        </h1>
+      </div>
+
+      <img src='original_64d92446026a5b2414276869cd6fe440_3.gif' className='backdrop' style={{maxWidth: '1000px'}}>
+      </img>
     </div>
+  
     
   )
 }
